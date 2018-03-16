618ca84f0fbf93992abd7d6c59bcc9fd81bd5f08
Peter Mazin <MaiznPetr@gmail.com>
2018-03-17 04:52:22 +0500

    Фикс бага в edge + начало луна

--- app/Scripts/Lun.jsx
+++ app/Scripts/Lun.jsx
@@ -5,28 +5,29 @@
 import React from 'react';
 import { Lang } from '../lang.jsx';
 export class Lun extends React.Component {
-    render() {
-        return (
-            <div className="deserted">
-                {Lang('deserted')}
-            </div>);
-    }
 
     //**Базовый алгоритм расчёта контрольного чила луна */
-    getLuhn(str) {
+    getLuhn(val) {
+        let str = (val || '').replace(/[\s]+/gi, '');
+        let sum = 0;
+
         for (let i = 0; i < value.length; i++) {
             let number = parseInt(value.charAt(i), 10);
             if (i % 2 != 0)
                 number *= 2;
-
             str = str + number;
         }
-
-        let sum = 0;
         for (let i = 0; i < str.length; i++) {
             let num = parseInt(str.charAt(i), 10);
             sum += num;
         }
         return sum;
+    }
+
+    render() {
+        return (
+            <div className="deserted">
+                {Lang('deserted')}
+            </div>);
     }
 }
